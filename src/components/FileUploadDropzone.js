15833f9b39508f74ba839a16252e4fcfbcc6f771
Kivi Corn <kivi.webdev@gmail.com>
2024-02-12 17:32:29 +0000

    init project.

---
+++ src/components/FileUploadDropzone.js
@@ -0,0 +1,63 @@
+'use client'
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useDropzone } from "react-dropzone";
+
+// https://react-dropzone.js.org/
+export default function FileUploadDropzone(props) {
+  const [files, setFiles] = useState([]);
+  const {getRootProps, getInputProps} = useDropzone({
+    accept: {
+      'image/*': []
+    },
+    // maxFiles:2,
+    onDrop: acceptedFiles => {
+      setFiles(acceptedFiles.map(file => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      })))
+    }
+  })
+  
+  const thumbnails = (
+    <div className="flex space-x-4 mt-10">
+      {files.map(file => (
+        <div key={file.name}>
+          <Image
+            src={ file.preview }
+            // Revoke data uri after image is loaded
+            onLoad={() => { URL.revokeObjectURL(file.preview) }}
+            alt=""
+            width={ 100 }
+            height={ 100 }
+            objectFit="cover"
+          />
+          { (file.size / (1024 * 1024)).toFixed(2) } MB
+        </div>
+      ))}
+    </div>
+  )
+
+  useEffect(() => {
+    // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+    return () => files.forEach(file => URL.revokeObjectURL(file.preview));
+  }, [])
+
+  return (
+    <section className="container mx-auto mt-12" {...getRootProps()}>
+      <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+          <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+          </svg>
+          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+          <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+        </div>
+        <input {...getInputProps()} />
+      </label>
+      <div>
+        { thumbnails }
+      </div>
+    </section>
+  );
+}
